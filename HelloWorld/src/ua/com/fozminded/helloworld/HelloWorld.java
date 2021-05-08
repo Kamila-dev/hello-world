04ee91bb35e39f5abf3df091984bc2f6640294dc
Kamila Gapizova <kamilagapiz@gmail.com>
2021-05-08 01:04:05 -0500

    initial checking hello world

---
+++ HelloWorld/src/ua/com/fozminded/helloworld/HelloWorld.java
@@ -0,0 +1,35 @@
+package ua.com.fozminded.helloworld;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+
+		int i = 6;
+		int j = 7;
+		float answer = (float)j / (float)i ;
+		String StringAnswer ;
+		
+
+	
+	String message = "Hello World!";
+	
+	
+	StringAnswer = message + " " + answer;
+	
+	
+	boolean isMoreThanOne;
+	if(answer > 1) {
+		isMoreThanOne = true;
+	}else{
+		isMoreThanOne = false;
+	}
+	
+	
+	
+	System.out.println(message);
+
+	System.out.println(StringAnswer + " " + isMoreThanOne); 		
+	}
+	
+
+}
